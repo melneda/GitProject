e45fa5d464f9283115344c67485eae66aa78f7f1
melneda <60905133+melneda@users.noreply.github.com>
2021-01-14 16:52:03 -0500

    secoond commit

--- src/Git_Practice.java
+++ src/Git_Practice.java
@@ -1,5 +1,11 @@
+import java.util.MissingFormatArgumentException;
 
 public class Git_Practice {
   // Hello eveyone 
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		
+	}
 
 }
